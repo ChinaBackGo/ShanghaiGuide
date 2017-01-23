0e86367d72ad5ce2d8ceeb108cad84aeb0b06cb4
ChinaBackGo <ChinaBackGo@users.noreply.github.com>
2017-01-23 21:30:32 +0800

    Udacity Multiscreen Apps Lesson 5: Project
    AndroidManifest.xml:
    for SeeActivity and EatActivity
    - added parentActivityName to allow for built in back arrow to return to parent activity
    
    MainActivity.java:
    - added openActivity helper class
    - added onClick listener for places_to_eat and places_to_see TextView
    - Linked onClick listener to open corresponding activity
    
    activity_main.xml
    - changed ordering of frames to put places_to_see first
    
    strings.xml
    - added category see and eat to strings
    
    Added Blank Activities EatActivity and SeeActivity
    	new file:   app/src/main/java/com/android/example/shanghaiguide/EatActivity.java
    	new file:   app/src/main/java/com/android/example/shanghaiguide/SeeActivity.java
    	new file:   app/src/main/res/layout/activity_eat.xml
    	new file:   app/src/main/res/layout/activity_see.xml

--- app/src/main/java/com/android/example/shanghaiguide/MainActivity.java
+++ app/src/main/java/com/android/example/shanghaiguide/MainActivity.java
@@ -1,13 +1,50 @@
 package com.android.example.shanghaiguide;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Logging TAG
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Set Places to see activity onClickListener
+        TextView seeTextView = (TextView) findViewById(R.id.places_to_see);
+        seeTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity(view, SeeActivity.class);
+            }
+        });
+
+        //Set Places to eat activity onClickListener
+        TextView eatTextView = (TextView) findViewById(R.id.places_to_eat);
+        eatTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity(view, EatActivity.class);
+            }
+        });
+    }
+
+    /**
+     * Helper class to open an Activity Class
+     * @param view not used - TODO:remove this?
+     * @param cls a class to be opened
+     */
+    public void openActivity (View view, Class cls) {
+        Intent intent = new Intent(this, cls);
+        startActivity(intent);
+        Log.i(TAG, "OpeningActivity: " + cls.getCanonicalName());
     }
 }
+
