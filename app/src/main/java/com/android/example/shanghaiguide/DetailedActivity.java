66fe56a19e492eb072fac6715efb5d2b2c7983d4
ChinaBackGo <ChinaBackGo@users.noreply.github.com>
2017-03-02 20:51:44 +0800

    Udacity Multiscreen Apps  Lesson 5: Project
    Added Bundle to intent called by Eat/See activity using putExtra()
    Using Logging - tested Bundle is passed to DetailedActivity
    
    DetailedActivity.java:
    Get Bundle and log latitude/longitude
    
    EatActivity.java/SeeActivity.java:
    
    create a bundle in listview onItemClickListener()
    add  values for longitude/latitude to bundle
    put bundle in intent with putExtra()

--- app/src/main/java/com/android/example/shanghaiguide/DetailedActivity.java
+++ app/src/main/java/com/android/example/shanghaiguide/DetailedActivity.java
@@ -2,12 +2,20 @@ package com.android.example.shanghaiguide;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class DetailedActivity extends AppCompatActivity {
+    //Logging TAG
+    private static final String TAG = "DetailedActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detailed);
+
+        Bundle intentBundle = new Bundle(getIntent().getBundleExtra("location"));
+        Log.v(TAG, "onCreate: Bundle: " + intentBundle.getDouble("latitude") + "/" +
+                intentBundle.getDouble("longitude"));
+
     }
 }
--- app/src/main/java/com/android/example/shanghaiguide/EatActivity.java
+++ app/src/main/java/com/android/example/shanghaiguide/EatActivity.java
@@ -57,9 +57,24 @@ public class EatActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                // parent here is the view that was clicked on in the listView
+                // getItemAtPosition returns the Place object associated with that view
                 Place currentPlace = (Place) parent.getItemAtPosition(position);
                 Log.v(TAG, "onItemClick: " + currentPlace.toString());
+
+                // Get the Place data and create a bundle
+                double latitude = currentPlace.getLatitude();
+                double longitude = currentPlace.getLongitude();
+                Bundle locationBundle = new Bundle();
+                locationBundle.putDouble("latitude", latitude);
+                locationBundle.putDouble("longitude", longitude);
+
+                Log.v(TAG, "onItemClick: locationBundle " + locationBundle.toString());
+
+                // Put the bundle in the intent
                 Intent intent = new Intent(view.getContext(), DetailedActivity.class);
+                intent.putExtra("location", locationBundle);
                 startActivity(intent);
             }
         });
