20e4ae2d248e9eb26e86a5e9d70771cfc83f465a
ChinaBackGo <ChinaBackGo@users.noreply.github.com>
2017-03-04 21:19:10 +0800

    Udacity Multiscreen Apps  Lesson 5: Project
     DetailActivity.java
     Remove unneeded import statement

--- app/src/main/java/com/android/example/shanghaiguide/DetailedActivity.java
+++ app/src/main/java/com/android/example/shanghaiguide/DetailedActivity.java
@@ -1,6 +1,5 @@
 package com.android.example.shanghaiguide;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
