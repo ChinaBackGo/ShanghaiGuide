e825f2883898e581e82a650d900e43aacdf0117e
ChinaBackGo <ChinaBackGo@users.noreply.github.com>
2017-10-22 00:15:15 +0800

    Udacity Multiscreen Apps Lesson 5: Project
    The activity code for querying the db for place array is now moved to a helper method in PlaceDbAssetHelper.java
    
    EatActivity.java:
    Refactored all db query quote to use getArrayOfPlaces() helper method
    Added some additional comments to help clarify private member variables
    
    SeeActivity.java:
    Places array list is no longer filled by using values in code, moved to a db query using
    getArrayOfPlaces() helper method in PlaceDbAssetHelper.java
    Added some additional comments to help clarify private member variables
    
    PlaceDbAssetHelper.java:
    Added getArrayOfPlaces helper method
    This method takes a db, context and category string and returns an array of all places from that query
    in an ArrayList

--- app/src/main/java/com/android/example/shanghaiguide/EatActivity.java
+++ app/src/main/java/com/android/example/shanghaiguide/EatActivity.java
@@ -18,12 +18,14 @@ public class EatActivity extends AppCompatActivity {
 
     //Logging TAG
     private static final String TAG = "EatActivity";
+    //Activity name
     private static final String ACTIVITY_NAME = "EatActivity";
+    //Category DB id TODO: bring this in from config?
     private static final String CATEGORY = "1";
 
     //Activity Color
     private final int mActivityColor = R.color.category_eat;
-    //PlaceDbAssetHelper mDbHelper = new PlaceDbAssetHelper(this);
+    //asset helper object
     private PlaceDbAssetHelper mDbHelper;
 
     @Override
@@ -31,86 +33,14 @@ public class EatActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.place_list);
         Log.v(TAG, "onCreate:");
-        Log.v(TAG, "Database Helper Test");
-        mDbHelper = new PlaceDbAssetHelper(this);
 
-        // Gets the data repository in write mode
+        // Get the database
+        mDbHelper = new PlaceDbAssetHelper(this);
         SQLiteDatabase db = mDbHelper.getReadableDatabase();
 
-        // Define a projection that specifies which columns from the database
-        // you will actually use after this query.
-        String[] projection = {
-                PlaceEntry._ID,
-                PlaceEntry.COLUMN_NAME_CATEGORY,
-                PlaceEntry.COLUMN_NAME_DESCRIPTION_BRIEF,
-        //      PlaceEntry.COLUMN_NAME_DESCRIPTION_DETAILED,
-                PlaceEntry.COLUMN_NAME_IMAGE_THUMB_ID,
-        //      PlaceEntry.COLUMN_NAME_IMAGE_ID,
-                PlaceEntry.COLUMN_NAME_RATING,
-                PlaceEntry.COLUMN_NAME_ADDRESS,
-                PlaceEntry.COLUMN_NAME_LATITUDE,
-                PlaceEntry.COLUMN_NAME_LONGITUDE
-        };
-
-        // Filter results WHERE "title" = 'My Title'
-        String selection = PlaceEntry.COLUMN_NAME_CATEGORY + " = ?";
-        String[] selectionArgs = { CATEGORY };
-
-        // How you want the results sorted in the resulting Cursor
-        String sortOrder =
-                PlaceEntry.COLUMN_NAME_DESCRIPTION_BRIEF  + " DESC";
-
-        Cursor cursor = db.query(
-                PlaceEntry.TABLE_NAME,                     // The table to query
-                projection,                               // The columns to return
-                selection,                                // The columns for the WHERE clause
-                selectionArgs,                            // The values for the WHERE clause
-                null,                                     // don't group the rows
-                null,                                     // don't filter by row groups
-                sortOrder                                 // The sort order
-        );
-
-
-        //Array list of places
-        ArrayList<Place> places = new ArrayList<>();
-
-        //Query DB for places data
-        while(cursor.moveToNext()) {
-            long itemId = cursor.getLong(
-                    cursor.getColumnIndexOrThrow(PlaceEntry._ID));
-            String db_descriptionBrief = cursor.getString(
-                    cursor.getColumnIndexOrThrow(PlaceEntry.COLUMN_NAME_DESCRIPTION_BRIEF));
-            //The R.drawable is an int returned by the R class, we can't store that in the DB, so
-            //we store the R.drawable.NAME instead, and use getResources to return the INT for the
-            //Image id associated with the R class. TODO: better way to do this?
-            String db_image_thumb_id = cursor.getString(
-                    cursor.getColumnIndexOrThrow(PlaceEntry.COLUMN_NAME_IMAGE_THUMB_ID));
-            int imageResource = this.getResources().getIdentifier(db_image_thumb_id, "drawable", this.getPackageName());
-
-            int db_rating = cursor.getInt(
-                    cursor.getColumnIndexOrThrow(PlaceEntry.COLUMN_NAME_RATING));
-            String db_address = cursor.getString(
-                    cursor.getColumnIndexOrThrow(PlaceEntry.COLUMN_NAME_ADDRESS));
-            double db_latitude = cursor.getDouble(
-                    cursor.getColumnIndexOrThrow(PlaceEntry.COLUMN_NAME_LATITUDE));
-            double db_longitude = cursor.getDouble(
-                    cursor.getColumnIndexOrThrow(PlaceEntry.COLUMN_NAME_LONGITUDE));
-
-            //Add places to array NOTE: imageResources is returned from db_image_thumb_id
-            places.add(new Place(db_descriptionBrief, imageResource, db_rating,
-                    db_address, db_latitude, db_longitude));
-        }
-        cursor.close();
-
-        //Add place object to places arrayList
-        /*
-        places.add(new Place("Boxing Cat Brewery (Yongfu Lu)", R.drawable.boxing_cat_yongfu_thumb, 1,
-                "复兴路82号, 近永福路", 31.2108309, 121.4408615));
-        places.add(new Place("Drink UP Beer Convenience Store", R.drawable.drink_up_thumb, 2,
-                "建国中路169号, 近瑞金二路", 31.2089731,121.4658453));
-        places.add(new Place("Uptown Records n' Beer", R.drawable.up_town_beer_thumb, 3,
-                "永福路131号, 近复兴路", 31.2099268,121.442875));
-        */
+        //Array list of places from db
+        // TODO: remove unchecked assignment (java warning)
+        ArrayList<Place> places = mDbHelper.getArrayOfPlaces(db, this, CATEGORY);
 
         // Create an {@link PlaceAdapter}, whose data source is a list of place objects. The
         // adapter knows how to create layouts for each item in the list, using the
